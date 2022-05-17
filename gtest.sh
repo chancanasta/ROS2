f733373d77a327149b384578c21f0a70fdb34b2e
chancanasta <brian.lewis1@gmail.com>
2022-05-17 17:34:23 +0100

    added scripts

---
+++ gtest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gazebo --verbose /opt/ros/galactic/share/gazebo_plugins/worlds/gazebo_ros_diff_drive_demo.world
---
+++ labtest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ros2 launch rmf_demos_gz office.launch.xml 
